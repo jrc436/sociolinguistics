babbcc0b1b594ec6e191828a86d0c166344efe06
Jeremy Cole <jrc436@psu.edu>
2016-01-17 20:59:08 -0500

    changed formatter to also breakup files

--- WordPropagation/src/util/JsonLayer.java
+++ WordPropagation/src/util/JsonLayer.java
@@ -23,30 +23,52 @@ public class JsonLayer {
 	private JsonReader jr;
 	public JsonLayer(Path p) {
 		try {
-			jr = Json.createReader(new FileReader(preProcess(p).toFile()));
+			jr = Json.createReader(new FileReader(p.toFile()));
 		}
 		catch (FileNotFoundException fe) {
 			System.err.println("No readable file at path: "+p.toString()+" was found");
 			System.exit(1);
 		} 
 	}
-	private static Path preProcess(Path p) {
+	public static void preProcess(Path inp, Path outFolder) {
 		Scanner s;
 		Path toRet = null;
 		FileWriter fr1;
 		try {
-			s = new Scanner(p.toFile());
-			toRet = Paths.get(p.toString()+"-mod");
-			fr1 = new FileWriter(toRet.toFile());
-			fr1.write("["+System.getProperty("line.separator"));
+			int fileNum = 0;
+			String numAppend = String.format("%03d", fileNum);
+			s = new Scanner(inp.toFile());
+			toRet = outFolder.resolve(inp.getFileName()+"-mod"+numAppend);
+			
 			List<String> lines = new ArrayList<String>();
 			while (s.hasNextLine()) {
 				String line = s.nextLine();
 				if (line.trim().isEmpty()) {
 					continue;
 				}
-				lines.add(line); 
+				lines.add(line);
+				if (lines.size() >= 10000) {
+					fr1 = new FileWriter(toRet.toFile());
+					fr1.write("["+System.getProperty("line.separator"));
+					for (int i = 0; i < lines.size(); i++) {
+						if (i == lines.size() -1) {
+							fr1.write(lines.get(i)+System.getProperty("line.separator"));
+						}
+						else {
+							fr1.write(lines.get(i)+","+System.getProperty("line.separator"));
+						}
+					}
+					fr1.write("]");
+					fr1.close();
+					lines.clear();
+					fileNum++;
+					numAppend = String.format("%2d", fileNum);
+					toRet = outFolder.resolve(inp.getFileName()+"-mod"+numAppend);
+				}
 			}
+			//final write
+			fr1 = new FileWriter(toRet.toFile());
+			fr1.write("["+System.getProperty("line.separator"));
 			for (int i = 0; i < lines.size(); i++) {
 				if (i == lines.size() -1) {
 					fr1.write(lines.get(i)+System.getProperty("line.separator"));
@@ -56,8 +78,8 @@ public class JsonLayer {
 				}
 			}
 			fr1.write("]");
-			s.close();
 			fr1.close();
+			s.close();
 		} 
 		catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -65,8 +87,8 @@ public class JsonLayer {
 		catch (IOException e) {
 			e.printStackTrace();
 		}
-		return toRet;
 	}
+
 	public List<Map<String, String>> getReadable() {
 		List<Map<String, String>> allMessages = new ArrayList<Map<String, String>>();
 		JsonArray ja = jr.readArray();
--- WordPropagation/src/wordcounter/CountMain.java
+++ WordPropagation/src/wordcounter/CountMain.java
@@ -1,30 +1,59 @@
 package wordcounter;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
-import java.io.FileWriter;
+
 import util.JsonLayer;
 import util.WordMap;
 
 public class CountMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		JsonLayer jl = new JsonLayer(Paths.get(args[0]));
-		List<Map<String, String>> jsons = jl.getReadable();
-		WordMap counts = new WordMap();
-		for (Map<String, String> json : jsons) {
-			counts.addSentence(json.get("body"));
-		}
-		try {
-			FileWriter fw = new FileWriter(args[1]);
-			fw.write(counts.toString());
-			fw.close();
-		}
-		catch (IOException ie) {
-			ie.printStackTrace();
+//		// TODO Auto-generated method stub
+//        String errMessage = "Required parameters: Input (JSON) and Output (Counts)";
+//        boolean needsConversion = true;
+//        if (args.length == 0 || args.length == 1) {
+//                System.out.println(errMessage);
+//                System.exit(1);
+//        }
+//        else if (args.length == 2) {
+//                System.out.println("Assuming value: "+needsConversion+" to determine whether conversion is needed");
+//        }
+//        else if (args.length == 3) {
+//                needsConversion = Boolean.valueOf(args[2]);
+//        }
+//        else {
+//                System.out.println("All arguments beyond three are being ignored");
+//        }
+//        JsonLayer jl = new JsonLayer(Paths.get(args[0]), needsConversion);
+//        List<Map<String, String>> jsons = jl.getReadable();
+//        WordMap counts = new WordMap();
+//        System.out.println("Beginning to add counts");
+//        int total = counts.size();
+//        for (Map<String, String> json : jsons) {
+//                counts.addSentence(json.get("body"));
+//                total--;
+//                if (total % 100 == 0) {
+//                        System.out.println(total + " remaining");
+//                }
+//        }
+//        try {
+//                FileWriter fw = new FileWriter(args[1]);
+//                fw.write(counts.toString());
+//                fw.close();
+//        }
+//        catch (IOException ie) {
+//                ie.printStackTrace();
+//        }
+
+		File folder = new File(args[0]);
+		File[] listOfFiles = folder.listFiles();
+		for (File bigRed : listOfFiles) {
+			JsonLayer.preProcess(bigRed.toPath(), Paths.get(args[1]));
 		}
 	}
 
