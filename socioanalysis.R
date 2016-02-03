19442c2266be45f25f6f7b98d76392a133f82374
Jeremy Cole <jrc436@psu.edu>
2016-02-03 00:21:43 -0500

    added more to the paper

--- socioanalysis.R
+++ socioanalysis.R
@@ -19,7 +19,7 @@ newpvars <- c("Intensity", "Casualities", "Age", "Refugees", "IDP", "SameYearIDP
 newdat2[newpvars] <- lapply(newdat2[newpvars],scale)
 
 model_externalities <- lmer(logscore ~ Casualities + IDP + Refugees + (1|Author) + (1|Subreddit), data=newdat)
-model_type <- lmer(logscore ~ Foreign + (1|Author) + (1|Subreddit), data=newdat)
+model_type <- lmer(logscore ~ Foreign + Separatist + (1|Author) + (1|Subreddit), data=newdat)
 model_intensity <- lmer(logscore ~ Intensity  + (1|Author) + (1|Subreddit), data=newdat)
 model_intensity_num <- lmer(logscore ~ Intensity  + (1|Author) + (1|Subreddit), data=newdat2)
 model_same_year <- lmer(logscore ~ SameYearFatalities + SameYearIDP + SameYearRefugees + (1|Author) + (1|Subreddit), data=newdat)
@@ -28,12 +28,13 @@ model_region <- lmer(logscore ~ Region + (1|Author) + (1|Subreddit), data=newdat
 model_externalities <- glmer(binscore ~ Casualities + IDP + (1|Author) + (1|Subreddit), data=newdat, family=binomial)
 
 model_externalities <- glmer(binscore ~ Refugees  + (1|Author) + (1|Subreddit), family=binomial, data=newdat)
-model_type <- glmer(binscore ~ Foreign + Criminal + Terrorism + Ethnic + Separatist + (1|Author) + (1|Subreddit), data=newdat,  family=binomial)
+model_type <- glmer(binscore ~ Separatist  + (1|Author) + (1|Subreddit), data=newdat,  family=binomial)
 model_intensity <- glmer(binscore ~ Intensity  + (1|Author) + (1|Subreddit), data=newdat,  family=binomial)
-model_intensity_num <- glmer(binscore ~ Intensity  + (1|Author) + (1|Subreddit), data=newdat2,  family=binomial)
+#model_intensity_num <- glmer(binscore ~ Intensity  + (1|Author) + (1|Subreddit), data=newdat2,  family=binomial)
 model_same_year <- glmer(binscore ~ SameYearFatalities + SameYearIDP + SameYearRefugees + (1|Author) + (1|Subreddit), data=newdat,  family=binomial)
 model_age <- glmer(binscore ~ Age + (1|Author) + (1|Subreddit), data=newdat,  family=binomial)
 model_region <- glmer(binscore ~ Region + (1|Author) + (1|Subreddit), data=newdat,  family=binomial)
+model_combo <- glmer(binscore ~ Age * Intensity + (1|Author) + (1|Subreddit), data=newdat,  family=binomial)
 
 
 model_contro <- lmer(Controversy ~ Casualities + (1|Author) + (1|Subreddit), data=newdat)
