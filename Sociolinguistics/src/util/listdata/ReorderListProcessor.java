b3e0d93cb7decb8ccf84c19a18e87c953de37d03
jrc436 <jrc436>
2016-09-08 17:10:06 -0400

    minor bugfixes to reorderlistprocessor

--- Sociolinguistics/src/util/listdata/ReorderListProcessor.java
+++ Sociolinguistics/src/util/listdata/ReorderListProcessor.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -25,27 +27,89 @@ public class ReorderListProcessor {
 		File outputDirectory = new File(args[1]);
 		String rebase = args.length > 2 ? args[2] : null;
 		List<FileParts> fp = captureAll(inputDirectory);
-		addRepaths(outputDirectory, fp, rebase);
+		//addRepaths(outputDirectory, fp, rebase);
+		Collections.sort(fp, FileParts.getFileNumComparator());
 		fp.get(0).readFile();
-		fp.get(0).writeFile();
+		//fp.get(0).writeFile();
+		int lastKeyLine = 0;
+		for (int i = 0; i < fp.get(0).lines.size(); i++) {
+			if (DataCollection.isKeyLine(fp.get(0).lines.get(i))) {
+				lastKeyLine = i;
+			}
+		}
 		for (int i = 1; i < fp.size(); i++) {
 			fp.get(i).readFile();
-			fixLists(fp.get(i-1).lines, fp.get(i).lines);
-			fp.get(i).writeFile();
+			lastKeyLine = fixLists(fp.get(i-1).lines, fp.get(i).lines, lastKeyLine);
+			if (lastKeyLine == -1) {
+				fp.remove(i);
+				i--;
+			}
+			//fp.get(i).writeFile();
 		}
+		for (int i = 0; i < fp.size(); i++) {
+			fp.get(i).fileNum = i;
+		}
+		addRepaths(outputDirectory, fp, rebase);
+		for (FileParts f : fp) {
+			f.writeFile();
+		}
+		//fp.get(0).writeFile();
+		//for (int i = 1; i < fp.size(); i++) {
+		//	fp.get(i).readFile();
+		//	fixLists(fp.get(i-1).lines, fp.get(i).lines);
+		//	fp.get(i).writeFile();
+		//}
 		//copyAll(fp);
 	}
-	private static void fixLists(List<String> first, List<String> second) {
+	private static int fixLists(List<String> first, List<String> second, int lastKeyLine1) {
 		if (!DataCollection.isKeyLine(first.get(0))) {
 			throw new IllegalArgumentException("The 'first' list has still not been fixed, so this call is invalid");
 		}
-		for (String s : second) {
-			if (DataCollection.isKeyLine(s)) {
+		int firstKeyLine = 0;
+		int lastKeyLine2 = -1;
+		while (true) {
+			if (firstKeyLine >= second.size()) {
 				break;
 			}
-			first.add(second.remove(0));
+			if (DataCollection.isKeyLine(second.get(firstKeyLine))) {
+				lastKeyLine2 = firstKeyLine;
+			}
+			if (lastKeyLine2 == -1) {
+				firstKeyLine++;
+			}
 		}
+		//if there are more of this type of element in first than in second, append to first
+		//otherwise, append the ones from first to second
+		if (first.size() - lastKeyLine1 > firstKeyLine) {
+			for (int i = 0; i < firstKeyLine; i++) {
+				//append all of the ones before the keyline to the first one
+				first.add(second.remove(0));
+			}
+		}
+		else {
+			for (int i = lastKeyLine1; i < first.size(); i++) {
+				second.add(i - lastKeyLine1, first.remove(lastKeyLine1));
+			}
+		}
+		return lastKeyLine2;
 	}
+
+//	private static void fixLists(List<String> first, List<String> second) {
+//		if (!DataCollection.isKeyLine(first.get(0))) {
+//			throw new IllegalArgumentException("The 'first' list has still not been fixed, so this call is invalid");
+//		}
+//		int j = 0;
+//		while (true) {
+//			if (DataCollection.isKeyLine(second.get(j))) {
+//				break;
+//			}
+//			j++;
+//		}
+//		for (int i = 0; i < j; i++) {
+//			//append all of the ones before the keyline to the first one
+//			first.add(second.remove(0));
+//		}
+//	}
 	public static List<FileParts> captureAll(File inputDirectory) {
 		List<FileParts> fileparts = new ArrayList<FileParts>();
 		for (File f : inputDirectory.listFiles()) {
@@ -77,43 +141,93 @@ public class ReorderListProcessor {
 	private static FileParts capture(File f) {
 		String fileName = f.getName();
 		String[] dashParts = fileName.split("-");
-		String numAndExt = dashParts[dashParts.length-1];
+		String numAndExt = "";
 		String baseName = "";
-		for (int i = 0; i < dashParts.length-1; i++) {
-			baseName += dashParts[i] + "-";
+		if (dashParts.length >= 2) {
+			numAndExt = dashParts[dashParts.length-1];
+			for (int i = 0; i < dashParts.length-1; i++) {
+				baseName += dashParts[i] + "-";
+			}
+			baseName = baseName.substring(0, baseName.length()-1);
 		}
-		baseName = baseName.substring(0, baseName.length()-1);
-		String[] extParse = numAndExt.split(".");
-		System.err.println("Warning: multiple periods in file extension. Parsing could be incorrect");
-		int num = Integer.parseInt(extParse[0]);
+		else {
+			System.err.println("Warning: no dashes found in file. Parsing could be incorrect (parsed from : "+fileName+")");
+			if (!fileName.contains(".")) {
+				return new FileParts(fileName, "", -1, f);
+			}
+			String[] parts = fileName.split("\\.");
+			baseName = parts[0];
+			for (int i = 1; i < parts.length; i++) {
+				numAndExt += parts[i];
+			}
+		}
+		String[] extParse = numAndExt.split("\\.");
+		if (extParse.length > 2) {
+			System.err.println("Warning: multiple periods in file extension. Parsing could be incorrect (parsed from: "+numAndExt+")");
+		}
+		else if (extParse.length < 2) {
+			System.err.println("Warning: no file extension found. Parsing could be incorrect (parsed from: "+numAndExt+")");
+			if (numAndExt.matches("\\d+")) {
+				return new FileParts(baseName, "", Integer.parseInt(numAndExt), f);
+			}
+			System.err.println("Warning: no number found. Parsing could be incorrect (parsed from: "+numAndExt+")");
+			return new FileParts(baseName, "", -1, f);
+		}
+		int num = extParse[0].matches("\\d+") ? Integer.parseInt(extParse[0]) : -1;
+		int s = num == -1 ? 0 : 1;
 		String ext = "";
-		for (int i = 1; i < extParse.length; i++) {
+		for (int i = s; i < extParse.length; i++) {
 			ext += extParse[i] + ".";
 		}
 		ext = ext.substring(0, extParse.length-1);
 		return new FileParts(baseName, ext, num, f);
-		
 	}
 	static class FileParts {
 		private final String baseName;
-		private final int fileNum;
+		private int fileNum;
 		private final String extension;
 		private final File origFile;
 		private Path repath;
 		private List<String> lines;
+		public static Comparator<FileParts> getFileNumComparator() {
+			return new Comparator<FileParts>() {
+				@Override
+				public int compare(FileParts o1, FileParts o2) {
+					if (o1.fileNum == o2.fileNum) {
+						return 0;
+					}
+					else if (o1.fileNum > o2.fileNum) {
+						return 1;
+					}
+					return -1;
+				}
+			};
+		}
 		public FileParts(String baseName, String extension, int fileNum, File origFile) {
 			this.baseName = baseName;
 			this.extension = extension;
 			this.fileNum = fileNum;
 			this.origFile = origFile;
 		}
+		private String extString() {
+			return extension == null || extension.isEmpty() ? "" : "."+extension;
+		}
 		public String toString() {
-			return baseName + "-" + fileNum + "." + extension;
+			if (fileNum == -1) {
+				return baseName + extString();
+			}
+			return baseName + "-" + fileNum  + extString();
 		}
 		public String format(int totalNumberOfFiles) {
-			int digitsNeeded = 1 + totalNumberOfFiles / 10;
+			if (fileNum == -1) {
+				if (totalNumberOfFiles != 1) {
+					System.err.println("Warning: naming scheme is assigning a non-numbered file, despite having multiple files");
+				}
+				return baseName + extString();
+			}
+			int digitsNeeded = 1 + ((int)Math.floor(Math.log10(totalNumberOfFiles)));
 			String formatNumber = String.format("%0"+digitsNeeded+"d", fileNum);
-			return baseName + "-" + formatNumber + "." + extension;
+			return baseName + "-" + formatNumber + extString();
 		}
 		public String rebaseFormat(String newBase, int totalNumberOfFiles) {
 			return new FileParts(newBase, extension, fileNum, origFile).format(totalNumberOfFiles);
@@ -123,7 +237,7 @@ public class ReorderListProcessor {
 				this.lines = Files.readAllLines(origFile.toPath());
 			}
 			catch (IOException ie) {
-				System.err.println("Unable tor ead file: "+origFile);
+				System.err.println("Unable to read file: "+origFile);
 				ie.printStackTrace();
 				System.exit(1);
 			}
