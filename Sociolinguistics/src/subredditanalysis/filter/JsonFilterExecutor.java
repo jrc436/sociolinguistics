7da3920ce82cc90f05505aa3e927f94603a8cc14
Jeremy Cole <jrc436@psu.edu>
2016-10-24 19:21:38 -0400

    updated build file, finished executor

--- Sociolinguistics/src/subredditanalysis/filter/JsonFilterExecutor.java
+++ Sociolinguistics/src/subredditanalysis/filter/JsonFilterExecutor.java
@@ -1,5 +1,18 @@
 package subredditanalysis.filter;
 
-public class JsonFilterExecutor {
+import util.json.JsonList;
+import util.sys.Executor;
+
+public class JsonFilterExecutor extends Executor<JsonFilter, JsonList, JsonList> {
+
+	public JsonFilterExecutor() {
+		super("jsonfilter", 40, JsonFilter.class, JsonList.class, JsonList.class);
+	}
+	
+	public static void main(String[] args) {
+		JsonFilterExecutor jfe = new JsonFilterExecutor();
+		jfe.initializeFromCmdLine(args);
+		jfe.run();
+	}
 
 }
