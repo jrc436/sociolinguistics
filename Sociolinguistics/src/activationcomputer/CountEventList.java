e607763fea893cfe240f62f4e8b5eab2a5d95337
jrc436 <jrc436@psu.edu>
2017-03-30 13:26:54 -0400

    fixed an error with period

--- Sociolinguistics/src/activationcomputer/CountEventList.java
+++ Sociolinguistics/src/activationcomputer/CountEventList.java
@@ -2,7 +2,6 @@ package activationcomputer;
 
 import java.time.Instant;
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -17,36 +16,35 @@ public class CountEventList extends GenericList<CountEvent> {
 	private static final long serialVersionUID = -5879822200172647270L;
 
 	
-	private final DoubleKeyMap<String, Period, Integer> wordsPerDay;
+	private final DoubleKeyMap<String, LocalDate, Integer> wordsPerDay;
 	private final ArrayList<ActivationEvent> store;
 	
 	public CountEventList() {
 		super();
-		this.wordsPerDay = new DoubleKeyMap<String, Period, Integer>();
+		this.wordsPerDay = new DoubleKeyMap<String, LocalDate, Integer>();
 		this.store = new ArrayList<ActivationEvent>();
 	}
 	public CountEventList(CountEventList other) {
 		super(other);
-		this.wordsPerDay = new DoubleKeyMap<String, Period, Integer>(other.wordsPerDay);
+		this.wordsPerDay = new DoubleKeyMap<String, LocalDate, Integer>(other.wordsPerDay);
 		this.store = new ArrayList<ActivationEvent>();
 	}
 	
 	public void addActEvent(ActivationEvent ae) {
-		Period p = fromInstant(ae.getUsageTime());
+		LocalDate p = fromInstant(ae.getUsageTime());
 		if (!wordsPerDay.containsKey(ae.getWord(), p)) {
 			wordsPerDay.put(ae.getWord(), p,  0);
 		}
 		wordsPerDay.put(ae.getWord(), p,  wordsPerDay.get(ae.getWord(), p)+1);
 		store.add(ae);
 	}
-	private static Period fromInstant(Instant in) {
+	private static LocalDate fromInstant(Instant in) {
 		LocalDate ld = in.atZone(ZoneId.systemDefault()).toLocalDate();
-		Period p = Period.between(ld, ld.plusDays(1));
-		return p;
+		return ld;
 	}
 	private void reduce() {
 		for (ActivationEvent ae : store) {
-			Period p = fromInstant(ae.getUsageTime());
+			LocalDate p = fromInstant(ae.getUsageTime());
 			int count = wordsPerDay.get(ae.getWord(), p);
 			this.add(new CountEvent(ae, count));
 		}
@@ -55,7 +53,7 @@ public class CountEventList extends GenericList<CountEvent> {
 	}
 	public void absorb(CountEventList other) {
 		this.store.addAll(other.store);
-		for (Pair<String, Period> key : other.wordsPerDay.keySet()) {
+		for (Pair<String, LocalDate> key : other.wordsPerDay.keySet()) {
 			if (this.wordsPerDay.containsKey(key)) {
 				this.wordsPerDay.put(key, this.wordsPerDay.get(key)+other.wordsPerDay.get(key));
 			}
